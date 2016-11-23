8e783a167303dd0bd85699d79665e8563ea39c6c
user <a>
2016-11-23 10:12:57 +0800

    v3

--- add2.php
+++ add2.php
@@ -7,9 +7,21 @@
         $stmt ->bindParam(1,$account);
         $stmt ->bindParam(2,$newPass);
         if($stmt->execute()){
-            echo  "ok";
+            echo 'OK';
+            // 以下進行檔案上傳處理
+            $newid = $pdo -> lastInsertId();
+            $upload = $_FILES['upload'];  // 上傳檔案的資訊 => 陣列
+            if($upload['error']==0){
+                if (copy($upload['tmp_name'], "upload/icon_{$newid}.jpg")){
+                    echo 'OK2';
+                }else{
+                    echo 'Copy Fail';
+                }
+            }else{
+                echo 'Upload Fail:' . $upload['error'];
+            }
         }else{
-            echo "XX";
+            echo 'XX';
         }
     }catch (Exception $e){
         die("server busy");
